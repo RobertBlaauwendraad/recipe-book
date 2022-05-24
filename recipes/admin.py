c62e4ca604433b5bcee1a6cbbb1de4d41c8b4474
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-05-24 13:57:33 +0200

    Admin section for recipe, created by Wicher

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredients, RecipeInstructions
 
-admin.site.register(Recipe)
+
+class RecipeIngredient(admin.TabularInline):
+    model = RecipeIngredients
+    extra = 1
+    verbose_name_plural = "Ingredients"
+
+
+class RecipeInstruction(admin.TabularInline):
+    model = RecipeInstructions
+    extra = 1
+    verbose_name_plural = "Instructions"
+    # exclude = ['step']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['recipe_name']}),
+        ('Information', {'fields': ['description', 'people', 'pub_date', ]}),
+    ]
+    inlines = [RecipeIngredient, RecipeInstruction]
+
+
+admin.site.register(Recipe, RecipeAdmin)
