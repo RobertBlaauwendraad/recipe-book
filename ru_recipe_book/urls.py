f3d49cd147b9d383902c08f2b452e09db2cb48e2
Bryan Rinders <Your-Email>
2022-06-09 08:45:00 +0200

    Users can now sign up for accounts

--- ru_recipe_book/urls.py
+++ ru_recipe_book/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('recipes/', include('recipes.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path('accounts/', include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
